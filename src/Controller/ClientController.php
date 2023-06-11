2706db3963ac4f39f99ecdfe1ebbac515ffd5568
Kone Kolotioloman Dieudonne <Kolotioloman.kone@rschain.net>
2023-06-11 12:50:59 +0000

    les 2 entites

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Form\ClientType;
 use App\Repository\ClientRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ClientController extends AbstractController
 {
     #[Route('/client', name: 'app_client')]
-    public function index(ClientRepository $clientRepository,Request $request): Response
+    public function index(ClientRepository $clientRepository): Response
     {
        
         
@@ -20,4 +23,62 @@ class ClientController extends AbstractController
         ]);
         
     }
+    #[Route('/newclient', name: 'app_client_add')]
+    public function new(Request $req,ManagerRegistry $doctrine): Response
+    {
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
+        if ($req->isMethod('POST'))
+            {
+                $form->handleRequest($req);
+                if ($form->isSubmitted() && $form->isValid())
+                {
+                    $em = $doctrine->getManager();
+                    $em->persist($client);
+                    $em->flush();
+                    $message='Le Client a ete ajoute avec succes';
+                }
+                return $this->render('client/ajouter.html.twig', [
+                    'form' => $form->createView(),
+                    'message'=> $message]);
+            }else {
+                return $this->render('client/ajouter.html.twig', [
+                    'form' => $form->createView(),
+                    'message'=> '']);
+            }
+        
+    }
+    #[Route('/editClient/{id}', name: 'app_client_edit', methods:["GET","POST"])]
+    public function edit(Request $req,ManagerRegistry $doctrine,Client $client): Response
+    {
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $doctrine->getManager();
+            $em->persist($client);
+            $em->flush();
+            $message='Le Client a ete ajoute avec succes';
+            return $this->redirectToRoute('app_client');
+        }
+
+        return $this->render('client/edit.html.twig', [
+            'client' => $client,
+            'form' => $form->createView(),
+        ]);
+       
+    }
+    #[Route('/deleteClient/{id}', name: 'app_client_del', methods:['GET','delete'])]
+    public function delete(Request $req,ManagerRegistry $doctrine,Client $client): Response
+    {
+       
+        $em = $doctrine->getManager();
+        $em->remove($client);
+        $em->flush();
+        
+
+        return $this->redirectToRoute('app_client');
+        
+    }
+   
 }
--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,17 +2,76 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use App\Repository\ProduitRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    #[Route('/produit', name: 'produit_liste')]
+    public function index(ProduitRepository $produit): Response
     {
         return $this->render('produit/index.html.twig', [
-            'controller_name' => 'ProduitController',
+            'produits' => $produit->findAll(),
         ]);
+    }
+    #[Route('/Ajout-produit', name: 'produit_new')]
+    public function new(Request $req, ManagerRegistry $doctrine): Response
+    {
+        $produit = new Produit();
+        $form = $this->createForm(ProduitType::class, $produit);
+        if ($req->isMethod('POST')) {
+            $form->handleRequest($req);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $doctrine->getManager();
+                $em->persist($produit);
+                $em->flush();
+                $message = 'Le Produit a ete ajoute avec succes';
+            }
+            return $this->render('produit/new.html.twig', [
+                'form' => $form->createView(),
+                'message' => $message
+            ]);
+        } else {
+            return $this->render('produit/new.html.twig', [
+                'form' => $form->createView(),
+                'message' => ''
+            ]);
+        }
+        return $this->render('produit/index.html.twig', []);
+    }
+    #[Route('/produit/{id}', name: 'produit_show', methods: ['GET', 'POST'])]
+    public function edit(Produit $produit, Request $req, ManagerRegistry $doctrine): Response
+    {
+
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($req);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $doctrine->getManager();
+            $em->persist($produit);
+            $em->flush();
+
+            return $this->redirectToRoute('produit_liste');
+        }
+        return $this->render('produit/edit.html.twig', [
+            'produit' => $produit,
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/delete/{id}', name: 'produit_delete', methods: ['GET', 'delete'])]
+    public function delete(Produit $produit, Request $req, ManagerRegistry $doctrine): Response
+    {
+
+
+        $em = $doctrine->getManager();
+        $em->remove($produit);
+        $em->flush();
+        return $this->redirectToRoute('produit_liste');
     }
 }
